49a513adcfef76e94e333169298d5e0ea17bb35f
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-25 20:57:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,9 +21,7 @@ function increaseRankBy(n) {
 
     for (let i = 0; i < child.length; i++) {
       var value = parseInt(child[i].innerHTML);
-      value = value + n;
-      return value.toString();
+      value = (value + n).toString();
     }
-    return lists;
   }
 }
